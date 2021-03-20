228fc785502bbcfe9068e5fb573e50aa9621b2ab
Muhammadnuman11 <80625784+Muhammadnuman11@users.noreply.github.com>
2021-03-20 04:29:28 -0700

    Add files via upload

---
+++ program.cs
@@ -0,0 +1,12 @@
+using System;
+namespace HelloWorld
+{
+    class program
+    {
+        static void Main(string[]args)
+        {
+            console.Writeline("hello World");
+            
+        }
+    }
+}
